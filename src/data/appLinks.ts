edc6601570d4a5b6bb693e5f00db0d8323caa8ea
Biraj <biraj.pub@gmail.com>
2026-05-01 13:21:58 +0530

    add brand page with colors & share page

---
+++ src/data/appLinks.ts
@@ -0,0 +1,2 @@
+export const APP_STORE_URL = "https://apps.apple.com/us/app/shellular/id6761985327";
+export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=io.foxbiz.shellular&pcampaignid=web_share";
