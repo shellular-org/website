657f8b798cd582e4bccbe3e5fa051318364e2149
Biraj <biraj.pub@gmail.com>
2026-05-09 19:15:50 +0530

    add meeting link for enterprise & beta badge

--- src/data/appLinks.ts
+++ src/data/appLinks.ts
@@ -1,2 +1,3 @@
 export const APP_STORE_URL = "https://apps.apple.com/us/app/shellular/id6761985327";
 export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=io.foxbiz.shellular";
+export const ENTERPRISE_CALENDAR_URL = "https://calendar.app.google/L6Dp71ePrBPyAmWV8";
