b1b883ae2849c61ade295b0714d234246fbcb190
Biraj <biraj.pub@gmail.com>
2026-06-23 22:49:22 -0700

    feat: update OpenSearch URL and improve structured data for SEO

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,17 +1,13 @@
-export function GET({ site }: { site?: URL }) {
-	if (!site) {
-		return new Response("Missing site URL", { status: 500 });
-	}
-
-	const body = `User-agent: *
+export function GET({ site }: { site: URL }) {
+  const body = `User-agent: *
 Allow: /
 
-Sitemap: ${new URL("/sitemap-0.xml", site).href}
+Sitemap: ${new URL("/sitemap-index.xml", site).href}
 `;
 
-	return new Response(body, {
-		headers: {
-			"Content-Type": "text/plain; charset=utf-8",
-		},
-	});
+  return new Response(body, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
 }
