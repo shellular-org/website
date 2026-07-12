c64580f19d4b25e5a799a61d4f9e97db174ee29e
Biraj <biraj.pub@gmail.com>
2026-07-11 23:40:11 -0700

    feat: enhance documentation and configuration for user account allowlist and VPS setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	// https://docs.astro.build/en/guides/integrations-guide/sitemap/
-	site: "https://shellular.dev",
-	integrations: [sitemap()],
+  // https://docs.astro.build/en/guides/integrations-guide/sitemap/
+  site: "https://shellular.dev",
+  redirects: {
+    "/vps": "/blog/how-to-set-up-shellular-on-a-vps",
+  },
+  integrations: [sitemap()],
+  compressHTML: false,
 });
