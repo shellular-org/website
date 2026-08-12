808f3bf5a4d15b3a104f50311c28769a4a90338a
Biraj <biraj.pub@gmail.com>
2026-08-11 20:04:04 -0700

    rename enterprise to talk to founder

--- src/data/appLinks.ts
+++ src/data/appLinks.ts
@@ -2,5 +2,5 @@ export const APP_STORE_URL =
 	"https://apps.apple.com/us/app/shellular/id6761985327";
 export const PLAY_STORE_URL =
 	"https://play.google.com/store/apps/details?id=io.foxbiz.shellular";
-export const ENTERPRISE_CALENDAR_URL =
+export const FOUNDER_CALENDAR_URL =
 	"https://calendar.app.google/L6Dp71ePrBPyAmWV8";
